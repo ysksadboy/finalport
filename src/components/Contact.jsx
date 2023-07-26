06b49ccd78c440c5e8758fd43fb60bf1a4463571
ysksadboy <yskorkys123@gmail.com>
2023-07-25 18:35:22 -0700

    init vite project

---
+++ src/components/Contact.jsx
@@ -0,0 +1,125 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import styled from "styled-components";
+
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  @media only screen and (max-width: 768px) {
+    justify-content: center;
+  }
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+  }
+`;
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: #E7ECEF;
+  border: none;
+  border-radius: 5px;
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #E7ECEF;
+`;
+
+const Button = styled.button`
+  background-color: #B6F2FF;
+  color: white;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Contact = () => {
+    const ref = useRef();
+    const [success, setSuccess] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            'service_5at10lo',
+            'template_50hiugu',
+            ref.current,
+            'Nj5PNS1MGYZOrro8Y'
+        )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    setSuccess(true);
+                },
+                (error) => {
+                    console.log(error.text);
+                    setSuccess(false);
+                }
+            );
+    };
+    return (
+        <Section>
+            <Container>
+                <Left>
+                    <Form ref={ref} onSubmit={handleSubmit}>
+                        <Title>Contact Me</Title>
+                        <Input placeholder="Name" name="from_name" rows={1} />
+                        <Input placeholder="Email" name="email_id" rows={1} />
+                        <TextArea
+                            placeholder="Write your message"
+                            name="message"
+                            rows={10}
+                        />
+                        <Button type="submit">Send</Button>
+                        {success &&
+                            "Your message has been sent. I will get back to you soon :)"}
+                    </Form>
+                </Left>
+                <Right>
+                </Right>
+            </Container>
+        </Section>
+    );
+};
+
+export default Contact;
